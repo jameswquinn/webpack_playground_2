80ca954def99742b2ef89af2657b7bde44085837
jameswquinn <jameswquinn2017@gmail.com>
2024-07-18 20:38:51 +0100

    initial setup

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import { h, render } from "preact";
+import { lazy, Suspense } from "preact/compat";
+import { Router, Route } from 'wouter';
+import Header from './components/Header';
+import './styles/main.scss';
+
+// Lazy load pages
+const Home = lazy(() => import('./pages/Home'));
+const About = lazy(() => import('./pages/About'));
+const NotFound = lazy(() => import('./pages/NotFound'));
+
+const App = () => (
+  <div>
+    <Header />
+    <Router>
+      <main>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Route path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route component={NotFound} />
+        </Suspense>
+      </main>
+    </Router>
+  </div>
+);
+
+render(<App />, document.body);
